5c42011e07dee310f7c2592433adc388fd54b2c8
shaibu13 <samedshaibu55@gmail.com>
2023-02-18 17:57:47 +0000

    4-puts.c, betty compliant

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 
+/**
+ * main - prints "/"Programming is like building a multilingual puzzle"
+ * /
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
