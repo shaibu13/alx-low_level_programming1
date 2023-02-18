8fe4afbf853c7bab60f2e4c02daa612c6f985547
shaibu13 <samedshaibu55@gmail.com>
2023-02-18 05:12:48 +0000

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Print alphabets in reverse
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char(pla[26]) = "zyxwvutsrqponmlkjihgfedcba";
+	int i;
+
+	for (i = 0; i < 26; ++i)
+	{
+		putchar(pla[26]);
+	}
+
+	putchar('\n');
+	return (0);
+}
