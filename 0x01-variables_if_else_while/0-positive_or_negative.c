b7c0103c869c03af17d0a43122800d2c7f35b6bf
shaibu13 <samedshaibu55@gmail.com>
2023-02-18 00:55:27 +0000

    0-positives_or_negatives, added a comment on main

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,7 +2,7 @@
 #include <time.h>
 #include <stdio.h>
 /**
-* Main - determinesis a number is positive, negative or zero
+* main - determinesis a number is positive, negative or zero
 *
 * Return: Always 0 (success)
 */
