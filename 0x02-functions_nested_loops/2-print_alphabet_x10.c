fae36ea389900a78d8cab0adbed5d5e507c56854
shaibu13 <samedshaibu55@gmail.com>
2023-02-23 01:31:33 +0000

    2-print_alphabet_x10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,7 +9,7 @@ void print_alphabet_x10(void)
 	char x[] = "abcdefghijklmnopqrstuvwxyz";
 	int i, j;
 
-	for (j = 0; j <= 10; j++)
+	for (j = 1; j <= 10; j++)
 	{
 		for (i = 0; x[i]; i++)
 		_putchar(x[i]);
---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,7 @@
+#include "main.h"
+/**
+ * _islower - 
+ */
+int _islower(int c)
+{
+}
