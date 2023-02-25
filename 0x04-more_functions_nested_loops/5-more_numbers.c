9208e3b4192b3794f5a975e3242840be86b73f25
shaibu13 <samedshaibu55@gmail.com>
2023-02-25 04:58:46 +0000

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * more_numbers - prints 10 times the numbers from 0-14
+ *
+ */
+
+void more_numbers(void)
+{	int a;
+	int i;
+
+	for (a = 0; a <= 9; a++)
+	{
+		for (i >= 0; i <= 14; i++)
+		{
+			_putchar();
+		}
+	}
+		putchar('\n');
+}:
