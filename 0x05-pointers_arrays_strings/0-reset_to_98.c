0007be55e23c083ecf39f8a11e122ba90c71c367
shaibu13 <samedshaibu55@gmail.com>
2023-02-28 02:06:37 +0000

    0-reset_to_98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * reset_to_98 - reset pointers value to 98
+ * @n: pointer variable
+ * Return: nothing
+ *
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+
+}
