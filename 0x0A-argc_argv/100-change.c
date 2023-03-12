f3c61b1f1b909e55baaa18f5d7cb9d68a731172c
shaibu13 <samedshaibu55@gmail.com>
2023-03-12 04:38:59 +0000

    100-change

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -6,7 +6,7 @@
 
 /**
  * coinConverter - Helper function that does all the mathematics
- * @i: Passed in variable from main for calculationss
+ * @i: Passed in variable from main for calculations
  * Return: The number of coins needed minimum for the passed in variable
  */
 int coinConverter(int i)
