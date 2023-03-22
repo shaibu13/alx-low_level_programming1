8180e1c1e1475879739f72079169e6442ebb5cdb
shaibu13 <samedshaibu55@gmail.com>
2023-03-22 03:27:35 +0000

    02-main

--- 0x0D-preprocessor/02-main.c
+++ 0x0D-preprocessor/02-main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - Prints the name of the file of the program
+ * main - Printss the name of the file of the program
  *        was compiled from, followed by a new line.
  *
  * Return: Always 0.
