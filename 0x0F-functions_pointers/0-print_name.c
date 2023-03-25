31001a83d7e11ac1a71b077ef9d0c235baedcb36
shaibu13 <samedshaibu55@gmail.com>
2023-03-25 00:59:03 +0000

    task completed

--- 0x0F-functions_pointers/0-print_name.c
+++ 0x0F-functions_pointers/0-print_name.c
@@ -1,7 +1,7 @@
 #include "function_pointers.h"
 
 /**
- * print_name - Prints a name.
+ * prinnt_name - Prints a name.
  * @name: The name to be printed.
  * @f: A pointer to a function that prints a name.
  */
